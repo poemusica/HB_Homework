4626f8df28014d02a93e1cb58a78d0f3ab7c26d5
Heather <heatheramahan@gmail.com>
2014-06-04 21:36:36 -0700

    initial commit of hw1 solution

---
+++ Homework01/newprocess.py
@@ -0,0 +1,10 @@
+def main():
+    my_file = open("um-server-01.log")
+    for line in my_file:
+        line = line.rstrip()
+        day = line[0:3]
+        if day == "Mon":
+            print line
+
+if __name__ == "__main__":
+    main()
